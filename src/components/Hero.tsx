45c53496a730d0ff9efcce3c6c9a85722bb880ac
Jay1156 <bharnejay127@gmail.com>
2025-06-04 02:26:54 +0530

    Update project files

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -92,7 +92,7 @@ const Hero: React.FC = () => {
           >
             <div className="relative w-[300px] h-[380px] bg-orange-500 rounded-2xl overflow-hidden">
               <img 
-                  src="/1000060333-removebg-preview.png"
+                  src="Public/1000060333-removebg-preview.png"
                 alt="Profile" 
                 className="w-full h-full object-cover"
               />
